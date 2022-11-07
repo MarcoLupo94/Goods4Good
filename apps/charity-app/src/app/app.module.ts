eefcb03cb24d5b4bd73d679440b5bcc6b987c133
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-07 17:17:03 +0000

    added Mongoose

--- apps/charity-app/src/app/app.module.ts
+++ apps/charity-app/src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { StoreModule } from '@ngrx/store';
-import { RoutingModule } from './routing.module';
 import { MaterialModule } from '@charity-app-production/material';
+import { StoreModule } from '@ngrx/store';
+import { AppComponent } from './app.component';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { RoutingModule } from './routing.module';
 
 @NgModule({
   declarations: [AppComponent, NxWelcomeComponent],
