ddaa249a0b455820e3003e9749095e7c10f35d16
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-09 17:17:44 +0000

    feat(router/guard): added auth guard to check if you are logged in
    If you are not logged in ,it's going to send you back to the welcome page

--- apps/charity-app/src/app/app-routing.module.ts
+++ apps/charity-app/src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { HomeComponent } from './home/home.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
-  { path: 'home', component: HomeComponent },
+  { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
---
+++ apps/charity-app/src/app/auth.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ):
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authService.isAuthenticated$.pipe(
+      tap((isAuthenticated: any) =>
+        !isAuthenticated ? this.router.navigateByUrl('/') : isAuthenticated
+      )
+    );
+  }
+}
