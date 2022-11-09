ac91cb8e8d55230f2cfc19ad12f0e0d93b86fa9c
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-09 15:46:36 +0000

    feat(api/interface): added swagger to test the api

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,27 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
-
+const configSwagger = (app) => {
+  // swagger to try api
+  const config = new DocumentBuilder()
+    .setTitle('API MAP')
+    .setDescription('The map API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  // swagger code
+};
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
   app.enableCors();
+  app.setGlobalPrefix(globalPrefix);
+
+  configSwagger(app);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
