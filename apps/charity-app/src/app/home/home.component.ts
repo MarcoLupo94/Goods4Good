0f62b8e46d38c1a03aee4fcc2ff4e0d33ceb1a19
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-09 19:15:01 +0000

    refactor(fake db): added fake db to the home component
    Need double checking this tomorrow / NEED TO MAP  TO THE RIGHT CHARITY TYPE

--- apps/charity-app/src/app/home/home.component.ts
+++ apps/charity-app/src/app/home/home.component.ts
@@ -1,5 +1,9 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Charity } from '@charity-app-production/api-interfaces';
+import { Observable } from 'rxjs';
 import { CharitiesApiService } from '../utils/charities-api.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'charity-app-production-home',
@@ -7,9 +11,18 @@ import { CharitiesApiService } from '../utils/charities-api.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-  constructor(private api: CharitiesApiService) {}
+  db: Charity[] = [];
+  constructor(private api: CharitiesApiService, private http: HttpClient) {}
 
-  ngOnInit(): void {
+  getData(): Observable<Charity[]> {
+    return this.http.get<Charity[]>('/assets/fakeDB.json');
+  }
+
+  ngOnInit(): any {
+    this.getData().subscribe((data) => {
+      this.db = [...data];
+      console.log(this.db);
+    });
     // this.api.getData().subscribe((data) => console.log(data));
   }
 }
