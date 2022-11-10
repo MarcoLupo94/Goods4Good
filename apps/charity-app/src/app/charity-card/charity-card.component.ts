e9da6f0b2756034daf35e675f1aab3457c2b802d
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-10 11:19:30 +0000

    added charity-card

--- apps/charity-app/src/app/charity-card/charity-card.component.ts
+++ apps/charity-app/src/app/charity-card/charity-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Charity } from '@charity-app-production/api-interfaces';
 
 @Component({
   selector: 'charity-app-production-charity-card',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharityCardComponent implements OnInit {
   constructor() {}
-
+  @Input()
+  charity!: Charity;
   ngOnInit(): void {}
 }
