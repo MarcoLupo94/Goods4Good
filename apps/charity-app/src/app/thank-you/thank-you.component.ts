3dc8e281c181e56b5c0b8a406278a963cac12a9e
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-11 19:07:19 +0000

    feat(thank you page): finished thank you page /Needs Image

--- apps/charity-app/src/app/thank-you/thank-you.component.ts
+++ apps/charity-app/src/app/thank-you/thank-you.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'charity-app-production-thank-you',
   templateUrl: './thank-you.component.html',
   styleUrls: ['./thank-you.component.css'],
 })
-export class ThankYouComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
+export class ThankYouComponent {
+  constructor(private router: Router) {}
+  navigate() {
+    this.router.navigate(['home']);
+  }
 }
