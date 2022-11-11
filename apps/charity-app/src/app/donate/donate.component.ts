e5b732c89fb91dbbb63afe40954665170b173621
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-11 17:31:34 +0000

    style(css): changed forms style

--- apps/charity-app/src/app/donate/donate.component.ts
+++ apps/charity-app/src/app/donate/donate.component.ts
@@ -1,14 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'charity-app-production-donate',
   templateUrl: './donate.component.html',
   styleUrls: ['./donate.component.css'],
 })
-export class DonateComponent implements OnInit {
+export class DonateComponent {
   constructor() {}
 
   handleSubmit() {}
-
-  ngOnInit(): void {}
 }
