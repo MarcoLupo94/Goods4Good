61e8cf212d5b5ef84894d7a4db16d07fce6e6266
MarcoLupo94 <lupo.marco94@gmail.com>
2022-11-15 17:44:55 +0000

    fix(fixe): fixed minor bug

--- apps/charity-app/src/app/utils/stripe.service.ts
+++ apps/charity-app/src/app/utils/stripe.service.ts
@@ -1,14 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-<<<<<<< HEAD
 import { Item } from '@charity-app-production/api-interfaces';
 import { environment } from '../../environments/environment';
 import { loadStripe } from '@stripe/stripe-js';
-=======
-import { Item } from '@charity-app-production/api-interfaces';
-import { environment } from '../../environments/environment';
-import { loadStripe } from '@stripe/stripe-js';
->>>>>>> 3f70613 (First commit)
 
 @Injectable({
   providedIn: 'root',
@@ -16,19 +10,6 @@ import { loadStripe } from '@stripe/stripe-js';
 export class StripeService {
   constructor(private http: HttpClient) {}
 
-<<<<<<< HEAD
-cartCheckout(cart: Item[]): void {
-  this.http
-    .post(environment.API_DB + 'checkout', {
-      items: cart,
-    })
-    .subscribe(async (res: any) => {
-      const stripe = await loadStripe(environment.STRIPE_KEY);
-      const id = res.session.id;
-      console.log(res.session.id);
-      stripe?.redirectToCheckout({
-        sessionId: res.id,
-=======
   cartCheckout(cart: Item[]): void {
     this.http
       .post(environment.API_DB + 'checkout', {
@@ -39,7 +20,7 @@ cartCheckout(cart: Item[]): void {
         const id = res.session.id;
         console.log(res.session.id);
         stripe?.redirectToCheckout({
-          sessionId: res.id,
+          sessionId: id,
         });
       });
   }
