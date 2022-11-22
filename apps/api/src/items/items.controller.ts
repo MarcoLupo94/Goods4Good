6f74c33529e7247393acbcd5501972b94e313bea
fabtroilo <fabtroilo@gmail.com>
2022-11-22 10:02:55 +0000

    Refactoring: organized security utility functions in specific file and imported functions for item security checks

--- apps/api/src/items/items.controller.ts
+++ apps/api/src/items/items.controller.ts
@@ -1,4 +1,4 @@
-import validator from "validator";
+import { validatePrice, sanitizeInputs } from "./items.security.checks";
 import { Item } from '@charity-app-production/api-interfaces';
 import {
   Controller,
@@ -20,23 +20,9 @@ export class ItemsController {
   @Post()
   create(@Body() item: Item) {
     try {
-      /* - Adding validation for price and escaping for all string inputs- */
-      function escapeAll(object) {
-        Object.keys(object).forEach(function (k) {
-          if (typeof object[k] === 'string' && k !== 'img_url') {
-            object[k] = validator.escape(object[k]);
-          }
-        })
-      }
-      escapeAll(item);
-      console.log(item);
-      if (!validator.isCurrency('' + item.price, {
-        require_symbol: false,
-        decimal_separator: '.',
-        allow_decimal: true,
-        require_decimal: false,
-        digits_after_decimal: [0, 1, 2]
-      })) throw 'Price format is not correct';
+      /* - Adding validation for price and escaping for all string properties- */
+      validatePrice(item);
+      sanitizeInputs(item);
       /* -------------------------- */
       return this.itemsService.create(item);
     } catch (error) {
