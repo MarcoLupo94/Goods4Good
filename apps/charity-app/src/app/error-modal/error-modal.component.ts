d450367de99fda49fad17cbe11932aca66d71770
chris3evans <chris3evans.ce@gmail.com>
2022-11-22 19:14:42 +0000

    Created error modal component

---
+++ apps/charity-app/src/app/error-modal/error-modal.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'charity-app-production-error-modal',
+  templateUrl: './error-modal.component.html',
+  styleUrls: ['./error-modal.component.css']
+})
+export class ErrorModalComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
